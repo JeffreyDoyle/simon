c458e2ae65a1cf6d8a067454cf6f815e8770b0fa
jeff.doyle@invokemedia.com <jeffreydoylec@gmail.com>
2017-08-07 16:35:38 -0700

    Implemented Simon Game

--- js/simon.js
+++ js/simon.js
@@ -19,6 +19,7 @@ function NoteBox(key, onClick) {
 	// Ensures that consequent plays won't prematurely remove the active class.
 	var playing = 0;
 
+	this.boxEl = boxEl;
 	this.key = key;
 	this.onClick = onClick || function () {};
 
@@ -55,6 +56,9 @@ function NoteBox(key, onClick) {
 
 		this.onClick(this.key)
 		this.play()
+
+
+
 	}.bind(this)
 
 	boxEl.addEventListener('mousedown', this.clickHandler);
@@ -67,10 +71,119 @@ function NoteBox(key, onClick) {
 // It will also demonstrate programmatically playing notes by calling play directly.
 var notes = {};
 
+// I want to use an array.
+var notesArray = [];
+
 KEYS.forEach(function (key) {
-	notes[key] = new NoteBox(key);
+	var box = new NoteBox(key);
+	notes[key] = box
+	notesArray.push(box);
 });
 
-KEYS.concat(KEYS.slice().reverse()).forEach(function(key, i) {
-	setTimeout(notes[key].play.bind(null, key), i * NOTE_DURATION);
-});
+// KEYS.concat(KEYS.slice().reverse()).forEach(function(key, i) {
+// 	setTimeout(notes[key].play.bind(null, key), i * NOTE_DURATION);
+// });
+
+
+function SimonGame(notes) {
+
+	// Are we currently in a game.
+	var gameInSession = false;
+	// The notes that will be used in this game.
+	var gameNotes = [];
+	// The index of the note in the game we're on.
+	var noteCounter = 0;
+	// Number of notes accumulator;
+	var numberOfNotes = 0;
+
+	this.startGame = () => {
+        console.log('startGame');
+
+		// Disable each notebox.
+        notes.forEach(function(note) {
+        	note.disable();
+		});
+
+		// Generates the first note for the game.
+		this.generateNote();
+
+		//Play the sequence of notes so far.
+       	this.playSequence();
+
+        // Enable each notebox.
+        notes.forEach(function(note) {
+            note.enable();
+        });
+
+		// The game is on.
+		gameInSession = true;
+	}
+
+	this.nextLevel = () => {
+        console.log('nextLevel');
+        noteCounter = 0;
+        // Disable each notebox.
+        notes.forEach(function(note) {
+            note.disable();
+        });
+
+        // Generates the first note for the game.
+        this.generateNote();
+
+        //Play the sequence of notes so far.
+        this.playSequence();
+
+        // Enable each notebox.
+        notes.forEach(function(note) {
+            note.enable();
+        });
+	}
+
+
+	this.playSequence = () => {
+        console.log('playSequence');
+        for (var i = 0; i < numberOfNotes; i++) {
+            setTimeout(gameNotes[i].play, i * NOTE_DURATION);
+        }
+	}
+
+	this.generateNote = () => {
+		console.log('generateNote');
+        for (var i = 0; i < 1; i++) {
+            gameNotes.push(notesArray[Math.floor(Math.random() * 3)]);
+            numberOfNotes++;
+        }
+	}
+
+	this.endGame = () => {
+		console.log('endGame');
+		// Reset the game.
+		gameInSession = false;
+        noteCounter = 0;
+        numberOfNotes = 0;
+        gameNotes = [];
+	}
+
+	this.clickHandler = (e) => {
+		console.log('click', e);
+		if (!e.target.id || e.target.id != gameNotes[noteCounter].key) {
+			this.endGame();
+		} else {
+			gameNotes[noteCounter].play();
+			noteCounter++;
+			// Check if the user has beat this level.
+			if (noteCounter === numberOfNotes) {
+                this.nextLevel();
+			}
+		}
+		console.log('cick', noteCounter, numberOfNotes);
+	}
+
+	notes.forEach((note) => {
+		note.boxEl.addEventListener('mousedown', this.clickHandler);
+	});
+}
+
+var game = new SimonGame(notesArray);
+game.startGame();
+
